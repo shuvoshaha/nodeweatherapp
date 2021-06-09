e8d47a3d7cd9d75f92b4ae3adc53aaa858ca5312
SKS <shaha460@gmail.com>
2021-06-10 04:21:44 +0600

    replace api data on html file

--- index.js
+++ index.js
@@ -4,24 +4,31 @@ const fs = require("fs");
 const http = require("http");
 const requests = require("requests");
 
-const homepage = fs.readFileSync("index.html")
+const homepage = fs.readFileSync("index.html", "utf-8");
+
+const repData = (tempVal, orgVal) =>{
+  let tempareture = tempVal.replace("{%temp%}", orgVal.main.temp);
+  tempareture = tempareture.replace("{%temMax%}", orgVal.main.temp_max)
+  return tempareture;
+}
 
 http.createServer((req, res) =>{
 if(req.url == '/'){
     requests("https://api.openweathermap.org/data/2.5/weather?q=joypurhat&appid=afa6a92c9ae3e721d26bad39b3de05c2")
     .on("data", function(chunk){
         const objData = JSON.parse(chunk);
-        const arr = [objData]
-        console.log(arr[0].main)
-        fs.writeFile("weather.txt", chunk, (err) =>{
-            if(err) throw err;
-            console.log("File updated")
-        })
-        
-        
+        const arrData = [objData]
+        // console.log(arr[0].main)
+        // fs.writeFile("weather.txt", chunk, (err) =>{
+        //     if(err) throw err;
+        //     console.log("File updated")
+        // })
+       const realData =  arrData.map((val) => repData(homepage, val) ).join("")
+       console.log(realData)
+  
     })
     .on("end", (err) =>{
-       console.log(err)
+        res.end()
     })
      
 } 
